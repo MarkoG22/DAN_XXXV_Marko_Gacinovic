936c5af7cc3c177df13f82c58583aad19578eb50
MarkoG22 <marko.gacinovic.el@gmail.com>
2020-06-19 11:55:59 +0200

    Play a game method

--- DAN_XXXV_Marko_Gacinovic/Zadatak_1/Program.cs
+++ DAN_XXXV_Marko_Gacinovic/Zadatak_1/Program.cs
@@ -13,5 +13,28 @@ namespace Zadatak_1
 
             Console.ReadLine();
         }
+
+        static void Play()
+        {
+            Console.Write("Please enter the number of players: ");
+            bool validPlayers = uint.TryParse(Console.ReadLine(), out uint players);
+
+            while (!validPlayers)
+            {
+                Console.Write("Wrong input, please try again: ");
+                validPlayers = uint.TryParse(Console.ReadLine(), out players);
+            }
+
+            Console.Write("Please enter the guessing number (1-100): ");
+            bool validNumber = uint.TryParse(Console.ReadLine(), out uint number);
+
+            while (!validNumber || !(number<101))
+            {
+                Console.Write("Wrong input, please try again: ");
+                validNumber = uint.TryParse(Console.ReadLine(), out number);
+            }
+
+            Console.WriteLine("User entered {0} players and guessing number is {1}.", players, number);
+        }
     }
 }
